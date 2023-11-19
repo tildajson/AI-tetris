2755ab50f61953e3aeb7ce8e7e1c18185a8399b1
Tilda Johansson <130234732+tildajson@users.noreply.github.com>
2023-11-19 18:55:03 +0100

    initial commit

---
+++ settings.py
@@ -0,0 +1,40 @@
+import pygame as pg
+import random
+
+vec = pg.math.Vector2
+
+FPS = 60
+FIELD_COLOR = (15, 15, 15)
+BG_COLOR = (36, 39, 35)
+
+FONT_PATH = 'assets/font/RetroGaming.ttf'
+
+ANIM_TIME_INTERVAL = 150  # milliseconds
+FAST_ANIM_TIME_INTERVAL = 50
+
+TILE_SIZE = 40
+FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+FIELD_SCALE_W, FIELD_SCALE_H = 1.7, 1.0
+WIN_RES = WIN_W, WIN_H, = FIELD_RES[0] * \
+    FIELD_SCALE_W, FIELD_RES[1] * FIELD_SCALE_H
+
+INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+NEXT_POS_OFFSET = vec(FIELD_W * 1.26, FIELD_H * 0.45)
+MOVE_DIRECTIONS = {'left': vec(-1, 0), 'right': vec(1, 0), 'down': vec(0, 1)}
+
+TETROMINOES = {
+    'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
+    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'I': [(0, 0), (0, 1), (0, -1), (0, -2)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (0, -1), (-1, -1)]
+}
+
+TETROMINO_COLORS = [[(149, 5, 197, 255)], [(206, 206, 1, 255)], [(3, 1, 198, 255)],
+                    [(202, 103, 3, 255)], [(1, 206, 209, 255)], [(0, 204, 0, 255)], [(203, 1, 3, 255)]]
+
+COLORS = random.choice(list(TETROMINO_COLORS))
